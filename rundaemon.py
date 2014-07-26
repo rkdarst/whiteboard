e38d03c024fd90171d33bdf49d32ee220d45e8a4
Richard Darst <rkd@zgib.net>
2014-07-26 19:17:13 +0300

    Add logging level option to rundaemon.py

--- rundaemon.py
+++ rundaemon.py
@@ -7,13 +7,19 @@ import mobwrite_core
 import mobwrite_daemon
 del sys.path[0:2]
 
+import getopt
+options, args = getopt.gnu_getopt(sys.argv[1:], "l:")
+options = dict(options)
+options.setdefault('-l', 'INFO')
+
+
 mobwrite_daemon.MAX_VIEWS = 100
 mobwrite_daemon.STORAGE_MODE = mobwrite_daemon.FILE
 mobwrite_daemon.LOCAL_PORT = 30711 # adjust
 
 mobwrite_core.MAX_CHARS = 30000
 mobwrite_core.TIMEOUT_TEXT = mobwrite_core.datetime.timedelta(days=31)
-mobwrite_core.LOG.setLevel(mobwrite_core.logging.DEBUG)
+mobwrite_core.LOG.setLevel(getattr(mobwrite_core.logging, options['-l']))
 
 # this is from __main__ in mobwrite_daemon
 mobwrite_core.logging.basicConfig()
